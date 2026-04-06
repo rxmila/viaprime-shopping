7d20caad5f3e8151f31e14fb1c248e850f2edce6
rxmila <rxmila1@gmail.com>
2026-04-05 21:53:58 -0300

    next.config

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // O Next.js gerencia o ESLint automaticamente. 
-  // Não precisamos de chaves extras aqui para o build básico.
-};
-
-export default nextConfig;
